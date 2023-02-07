cfc0ee4a75f4c59335e1b5cb06959a2cb18f20e1
ClintTheEngineer <81938100+ClintTheEngineer@users.noreply.github.com>
2023-02-07 15:07:39 -0500

    Add files via upload

--- input.js
+++ input.js
@@ -3,16 +3,18 @@ require('./decs')
 const url = require('json-api-to-array')
 
 module.exports = function array(url){
+    let arr;
     const request = new XMLHttpRequest();
     function onload(){
         const data = JSON.parse(this.response);
-        sessionStorage.setItem('push', JSON.stringify(data))
+        sessionStorage.setItem('push', JSON.stringify(data));
+        arr = JSON.parse(sessionStorage.getItem('push'))
     }
     request.open('GET', url, true);
     request.addEventListener('load', onload);
     request.send();
 
-    const arr = JSON.parse(sessionStorage.getItem('push'));
+    
     return JSON.stringify(arr);
 
 }
